581a0e9112b890fa82b799c40c51898c661a07e8
Pavel Zverkov <pavel_zverkov@MacBook-Air-Pavel.local>
2021-05-23 02:30:46 +0300

    Working click stopwatch

--- app/src/main/java/ru/zverkov_studio/split/AdapterPersonsEvent.java
+++ app/src/main/java/ru/zverkov_studio/split/AdapterPersonsEvent.java
@@ -4,6 +4,8 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 
@@ -24,15 +27,22 @@ public class AdapterPersonsEvent extends RecyclerView.Adapter<AdapterPersonsEven
     private static final String TABLE_DECLARED = "declared_table";
     ContentValues cv = new ContentValues();
 
-    ArrayList<Stopwatch> stopwatches = new ArrayList<>();
-
     private static final int PENDING_REMOVAL_TIMEOUT = 1000; // 3sec
 
     List<String> track;
     int point = 0;
     private Context mContext;
-    String item;
+
+    int item;
     Cursor mCursor;
+
+    long[][] times;
+    HashMap<Integer, long[]> times_array = new HashMap<>();
+    HashMap<Integer, Integer> on_track = new HashMap<>();
+    long start_time, start_lap_time;
+
+    ArrayList<Integer> position_click = new ArrayList<>();
+
     boolean undoOn = true;
     private Handler handler = new Handler(); // hanlder for running delayed runnables
     HashMap<String, Runnable> pendingRunnables = new HashMap<>(); // map of items to pending runnables, so we can cancel a removal if need be
@@ -42,34 +52,71 @@ public class AdapterPersonsEvent extends RecyclerView.Adapter<AdapterPersonsEven
         open_DB();
         mCursor = persons.getAllData(TABLE_DECLARED, null);
         track = events.get_track();
-        for (int i = 0; i < getItemCount(); i++) {
-            stopwatches.add(new Stopwatch());
+        times = new long[mCursor.getCount()][track.size()];
+        for (int i = 0; i < mCursor.getCount(); i++){
+            mCursor.moveToPosition(i);
+            item = mCursor.getInt(0);
+            times_array.put(item, new long[track.size()]);
+            on_track.put(item, 0);
+
         }
+        for (int key: on_track.keySet()){
+            Log.d("AdapterEvents", String.valueOf(key) + ": " + String.valueOf(on_track.get(key)));
+        }
+
     }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("AdapterEvents", "onCreate");
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_person_event, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("AdapterEvents", "onBind  " + String.valueOf(position));
         mCursor.moveToPosition(position);
-        item = mCursor.getString(mCursor.getColumnIndex(DataBasePersons.COLUMN_ID));
+        item = mCursor.getInt(0);
+        Log.d("AdapterEvents", "item  " + String.valueOf(item));
 
         holder.id = item;
         holder.number.setText(String.valueOf(position + 1));
         holder.sportsman_name.setText(mCursor.getString(mCursor.getColumnIndex(DataBasePersons.COLUMN_NAME)));
-        holder.main_time.setText(stopwatches.get(position).timer);
-        holder.lap_time.setText(stopwatches.get(position).lap_timer);
         holder.distance_point.setText("");
+        if (!position_click.contains(item)){
+            holder.stopwatch.stop_setting();
+            holder.main_time.setText("СТАРТ");
+            holder.lap_time.setText("");
+        }
+        else {
+            //start_time = times[position][0];
+            //start_lap_time = times[position][holder.point];
+            Log.d("AdapterEvents", "position " + String.valueOf(position));
+            Log.d("AdapterEvents", "point " + String.valueOf(on_track.get(item)));
+            start_time = times_array.get(item)[0];
+            start_lap_time = times_array.get(item)[on_track.get(item) - 1];
+
+            holder.stopwatch.run_setting(holder.main_time, holder.lap_time, start_time, start_lap_time);
+        }
         holder.button_time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopwatches.get(position).start();
-                holder.main_time.setText(stopwatches.get(position).timer);
-                holder.lap_time.setText(stopwatches.get(position).lap_timer);
+                mCursor.moveToPosition(position);
+                item = mCursor.getInt(0);
+                Log.d("AdapterEvents", "Click " + String.valueOf(position));
+                Log.d("AdapterEvents", "point_before_click " + String.valueOf(on_track.get(item)));
+                Log.d("AdapterEvents", "item_click " + String.valueOf(item));
+                //times[position][holder.point] = SystemClock.uptimeMillis();
+                times_array.get(item)[on_track.get(item)] = SystemClock.uptimeMillis();
+                on_track.put(item, on_track.get(item) + 1);
+
+                Log.d("AdapterEvents", "holder_point_after_click " + String.valueOf(on_track.get(item)));
+                position_click.add(item);
+                notifyItemChanged(position);
+                for (int key: on_track.keySet()){
+                    Log.d("AdapterEvents", String.valueOf(key) + ": " + String.valueOf(on_track.get(key)));
+                }
             }
         });
     }
@@ -90,9 +137,10 @@ public class AdapterPersonsEvent extends RecyclerView.Adapter<AdapterPersonsEven
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        String id;
+        int id, point;
         TextView sportsman_name, main_time, lap_time, distance_point, number;
         LinearLayout button_time;
+        Stopwatch stopwatch;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -102,6 +150,8 @@ public class AdapterPersonsEvent extends RecyclerView.Adapter<AdapterPersonsEven
             distance_point = itemView.findViewById(R.id.distance_point);
             number = itemView.findViewById(R.id.number);
             button_time = itemView.findViewById(R.id.catch_time);
+            stopwatch = new Stopwatch();
+            point = 0;
         }
     }
 
@@ -112,11 +162,4 @@ public class AdapterPersonsEvent extends RecyclerView.Adapter<AdapterPersonsEven
         events.open();
     }
 
-    public class updater {
-        ViewHolder mHolder;
-        Handler handler = new Handler();
-        public updater (ViewHolder holder) {
-            
-        }
-    }
 }
--- app/src/main/java/ru/zverkov_studio/split/Stopwatch.java
+++ app/src/main/java/ru/zverkov_studio/split/Stopwatch.java
@@ -6,35 +6,50 @@ import android.widget.TextView;
 
 public class Stopwatch {
 
-    String timer = "СТАРТ", lap_timer = "";
-    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
+    long MillisecondTime_main, MillisecondTime_lap, StartTime_main, StartTime_lap, UpdateTime = 0L ;
     Handler handler = new Handler();
     int Hours, Seconds, Minutes, MilliSeconds ;
+    TextView timer, lap_timer;
 
     public Stopwatch(){
-        StartTime = SystemClock.uptimeMillis();
     }
 
-    public void start() {
+    public void run_setting(TextView main_time, TextView lap_time, long start_time, long start_lap_time){
+        StartTime_main = start_time;
+        StartTime_lap = start_lap_time;
+        timer = main_time;
+        lap_timer = lap_time;
         handler.postDelayed(runnable, 0);
+    }
+
+    public void stop_setting() {
+        handler.removeCallbacks(runnable);
     }
 
     public Runnable runnable = new Runnable() {
 
         public void run() {
-            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
-            UpdateTime = TimeBuff + MillisecondTime;
+            MillisecondTime_main = SystemClock.uptimeMillis() - StartTime_main;
+            UpdateTime = MillisecondTime_main;
             Seconds = (int) (UpdateTime / 1000);
             Minutes = Seconds / 60;
             Hours = Minutes / 60;
             Seconds = Seconds % 60;
             MilliSeconds = (int) (UpdateTime % 10);
-            timer = (String.format("%02d", Hours) + ":"
+            timer.setText(String.format("%02d", Hours) + ":"
                     + String.format("%02d", Minutes) + ":"
                     + String.format("%02d", Seconds) + "."
                     + String.format("%d", MilliSeconds));
 
-            lap_timer = (String.format("%02d", Hours) + ":"
+            MillisecondTime_lap = SystemClock.uptimeMillis() - StartTime_lap;
+            UpdateTime = MillisecondTime_lap;
+            Seconds = (int) (UpdateTime / 1000);
+            Minutes = Seconds / 60;
+            Hours = Minutes / 60;
+            Seconds = Seconds % 60;
+            MilliSeconds = (int) (UpdateTime % 10);
+
+            lap_timer.setText(String.format("%02d", Hours) + ":"
                     + String.format("%02d", Minutes) + ":"
                     + String.format("%02d", Seconds) + "."
                     + String.format("%d", MilliSeconds));
